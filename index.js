0b991accd18c3d6f67f349a3c97c332cf92e7c42
Dagmawi <dagmawihm@gmail.com>
2023-11-30 17:10:20 -0500

    besic crud

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// index.js
-
 const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 // Import the Campground model from the module
 const Campground = require('./models/campground');
@@ -20,30 +19,92 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('index', { campgrounds });
 });
 
-app.get('/addcamp', (req, res) => {
-    console.log("starting to post");
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('new');
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
+
+    res.render('show', { campground });
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
+
+    res.render('edit', { campground });
+});
+
+app.patch('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const title = req.body.title;
+    const price = req.body.price;
+    const location = req.body.location;
+    const description = req.body.description;
+
+    const campground = await Campground.findByIdAndUpdate(
+        id,
+        { title: title, price: price, location: location, description: description },
+        { runValidators: true, new: true }
+    );
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const deletedCampground = await Campground.findByIdAndDelete(id);
+
+    res.redirect(`/campgrounds`);
+
+})
+
+
+
+app.post('/campgrounds', async (req, res) => {
+    const title = req.body.title;
+    const price = req.body.price;
+    const location = req.body.location;
+    const description = req.body.description;
 
     const camp = new Campground({
-        title: "yabede camp",
-        price: "100",
-        description: "are yabede camp nw nuna eyut",
-        location: "302 wayne ave, silver spring, md",
-    });
-
-    camp.save()
-        .then(camp => {
-            console.log(camp);
-            res.send(camp);
-        })
+        title: title,
+        price: price,
+        location: location,
+        description: description
+    })
+    camp.save().then()
         .catch(e => {
-            console.log(e);
-            res.status(500).send("Error saving camp");
-        });
-});
+            console.log(e)
+        })
+
+        res.redirect(`/campgrounds/${camp._id}`);
+})
+
+
+
+
+
+
+
+
+
+
 
 app.listen(8080, () => {
     console.log("LISTENING ON PORT 8080");
---
+++ seeder/index.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+
+const titles = require('./title');
+const descriptions = require('./descriptions');
+const cityStatePairs = require('./location');
+
+tlength = titles.length;
+dlength = descriptions.length;
+clength = cityStatePairs.length;
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp')
+    .then(() => {
+        console.log("CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!!!")
+        console.log(err)
+    });
+
+const seed = async () =>{
+    await Campground.deleteMany({});
+for (let a = 0; a<101; a++)
+{
+    const camp = new Campground({
+        title: titles[Math.floor(Math.random() * tlength)],
+        price: Math.floor(Math.random() * (300 - 50 + 1)) + 50,
+        description: descriptions[Math.floor(Math.random() * dlength)],
+        location: `${cityStatePairs[Math.floor(Math.random() * clength)].city}, ${cityStatePairs[Math.floor(Math.random() * clength)].state}`,
+    });
+    await camp.save()
+    .then(camp => {
+        console.log(camp);
+
+    })
+    .catch(e => {
+        console.log(e);
+    });
+}
+
+
+}
+
+seed().then(() => {
+    mongoose.connection.close()
+});
+
+
